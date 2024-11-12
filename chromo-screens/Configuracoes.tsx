281bd8ec37392c77bead60cb45c183fc1c999b12
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-11-11 22:30:58 -0300

    feat: Estruturando a página de configurações do chromodoro

--- chromo-screens/Configuracoes.tsx
+++ chromo-screens/Configuracoes.tsx
@@ -1,9 +1,51 @@
 import React from "react";
 
-import { Text } from "react-native";
+import { useState } from "react";
 
-export default function Configuracoes(){
-    return(
-        <Text>Início</Text>
+import { Text, View, StyleSheet, TextInput, Switch } from "react-native";
+
+import TituloIcone from "@/components/TituloIcone";
+import LabelInput from "@/components/LabelInput";
+import SwitchLabel from "@/components/SwitchLabel";
+
+export default function Configuracoes() {
+
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    return (
+        <View>
+
+            <Text>Configurações</Text>
+
+            <TituloIcone titulo="TIMER" icone="timer-outline" />
+
+            <View>
+
+                <View>
+                    <Text>Tempo (minutos)</Text>
+                </View>
+                <View>
+                    <LabelInput label="Pomodoro"/>
+
+                    <LabelInput label="Pausa curta"/>
+
+                    <LabelInput label="Pausa longa"/>
+                </View>
+
+
+            </View>
+
+            <View>
+                <SwitchLabel label="Iniciar pausa automaticamente "/>
+                <SwitchLabel label="Iniciar pomodoro automaticamente"/>
+            </View>
+
+            <TituloIcone titulo="ALARME" icone="timer-outline" />
+
+            <View>
+                <SwitchLabel label="Tocar alarme na troca de timers"/>
+            </View>
+        </View>
     )
 }
