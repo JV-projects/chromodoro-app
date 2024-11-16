1404fc647bc44c6912bc9d107f7a78e033aa9aed
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2024-11-16 16:53:36 -0300

    feat: Fazendo o timer funcionar

--- chromo-screens/Inicio.tsx
+++ chromo-screens/Inicio.tsx
@@ -1,9 +1,14 @@
 import Button from "@/components/Button";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView} from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+interface Tempo {
+    minutos: number,
+    segundos: number
+}
 
 export default function Inicio() {
 
@@ -11,39 +16,94 @@ export default function Inicio() {
     const [pausaCurtaAtiva, setPausaCurtaAtiva] = useState(false);
     const [pausaLongaAtiva, setPausaLongaAtiva] = useState(false);
 
+    const [play, setPlay] = useState(false)
+
     const [mensagem, setMensagem] = useState("");
 
-    const [tempo] = useState("00:00")
+    const [tempo, setTempo] = useState<Tempo>({ minutos: 0, segundos: 0 })
+
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
     const handleStateFalse = () => {
-
         setPomodoroAtivo(false)
         setPausaLongaAtiva(false)
         setPausaCurtaAtiva(false)
 
+        setPlay(false)
+
+        clearInterval(intervalRef.current as NodeJS.Timeout);
+        setTempo({ minutos: 0, segundos: 0 });
     }
 
     const handleIniciarPomodoro = () => {
         handleStateFalse()
         setPomodoroAtivo(true)
         setMensagem("Hora do foco!")
+
+        setTempo({ minutos: 0, segundos: 3 });
     }
 
     const handleIniciarPausaCurta = () => {
         handleStateFalse()
         setPausaCurtaAtiva(true)
         setMensagem("Hora da pausa...")
+
+        setTempo({ minutos: 0, segundos: 5 });
     }
 
     const handleIniciarPausaLonga = () => {
         handleStateFalse()
         setPausaLongaAtiva(true)
         setMensagem("Hora de uma boa pausa...")
+        setTempo({ minutos: 0, segundos: 1 });
     }
+
+    const iniciarTimer = () => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+        }
+
+        intervalRef.current = setInterval(() => {
+            setTempo(prevTempo => {
+
+                /* Se minutos e segundo forem === 0 */
+
+                if (prevTempo.minutos === 0 && prevTempo.segundos === 0) {
+                    clearInterval(intervalRef.current as NodeJS.Timeout);
+                    setMensagem("Tempo acabou!");
+                    return { minutos: 0, segundos: 0 };
+
+                    /* Se segundo forem === 0 reinicia a contagem de segundos*/
+                } else if (prevTempo.segundos === 0) {
+                    return { minutos: prevTempo.minutos - 1, segundos: 59 };
+
+                } else {
+                    /* Se segundo forem === 0 reinicia a contagem de segundos*/
+                    return { minutos: prevTempo.minutos, segundos: prevTempo.segundos - 1 };
+                }
+            });
+        }, 1000);
+    };
+
+    useEffect(() => {
+        if (play) {
+            iniciarTimer();
+        } else if (intervalRef.current) {
+            clearInterval(intervalRef.current)
+        }
+
+        return () => clearInterval(intervalRef.current as NodeJS.Timeout);
+
+    }, [play]);
+
+    const trocarPlay = () => {
+        setPlay(!play);
+      };
 
     return (
         <ScrollView>
             <View style={styles.viewInicio}>
+
                 <View style={styles.viewButtons}>
                     <Button onPress={handleIniciarPomodoro} label="Pomodoro" isClicado={pomodoroAtivo} />
                     <Button onPress={handleIniciarPausaCurta} label="Pausa curta" isClicado={pausaCurtaAtiva} />
@@ -55,13 +115,14 @@ export default function Inicio() {
                 </View>
 
                 <View style={styles.timerView}>
-                    <Text style={styles.timerText}>{tempo}</Text>
+                    <Text style={styles.timerText}>{String(tempo.minutos).padStart(2, '0')}:{String(tempo.segundos).padStart(2, '0')}</Text>
                 </View>
 
                 <View>
-                    <TouchableOpacity>
-                        <MaterialCommunityIcons name="play-circle-outline" size={80} color="#535353" />
-                        {/* <MaterialCommunityIcons name="pause-circle-outline" size={24} color="black" /> */}
+                    <TouchableOpacity onPress={trocarPlay}>
+                        {play ?
+                            (<MaterialCommunityIcons name="pause-circle-outline" size={80} color="#535353" />) :
+                            (<MaterialCommunityIcons name="play-circle-outline" size={80} color="#535353" />)}
                     </TouchableOpacity>
                 </View>
             </View>
@@ -85,9 +146,9 @@ const styles = StyleSheet.create({
     mensagem: {
         color: "#D1717B",
         fontSize: 24,
-        fontWeight: 600, 
+        fontWeight: 600,
     },
-    timerView:{
+    timerView: {
         borderWidth: 2,
         borderColor: "#D1717B",
         borderRadius: "100%",
@@ -96,7 +157,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    timerText:{
+    timerText: {
         fontSize: 40,
         fontWeight: 500,
         color: "#D1717B",
