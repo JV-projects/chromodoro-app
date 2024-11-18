c975e857cfa652f5f3c534a4f1ca867e450c5e35
Carjooj <victor@4info.com.br>
2024-11-18 00:12:15 -0300

    feat: Criando serviço para as requisições da API

--- service/api.ts
+++ service/api.ts
@@ -1,2 +1,38 @@
-import axios from 'axios'
+import axios, {AxiosResponse} from 'axios'
 
+export async function post<T, R>(body: T, url: string, token?: string, ): Promise<R> {
+    try {
+        const headers =
+            token ? {'Content-Type': 'application/json', Authorization: `Bearer ${token}` } : {'Content-Type': 'application/json'};
+
+        const resposta: AxiosResponse<R> = await axios.post(url, body, { headers });
+
+        return resposta.data;
+    } catch (erro: unknown) {
+        if (axios.isAxiosError(erro)) {
+            const mensagemErro = erro.response?.data || erro.message || "Erro desconhecido";
+
+            throw new Error(mensagemErro)
+        } else {
+            throw new Error("Erro inesperado")
+        }
+    }
+}
+
+export async function get<R>(token: string, url: string): Promise<R> {
+    try {
+        const headers = token ? { Authorization: `Bearer ${token}` } : {};
+
+        const resposta: AxiosResponse<R> = await axios.get(url, { headers })
+
+        return resposta.data;
+    } catch (erro: unknown) {
+        if (axios.isAxiosError(erro)) {
+            const mensagemErro = erro.response?.data || erro.message || "Erro desconhecido";
+
+            throw new Error(mensagemErro)
+        } else {
+            throw new Error("Erro inesperado")
+        }
+    }
+}
