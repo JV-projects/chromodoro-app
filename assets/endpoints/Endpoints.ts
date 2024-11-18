c1556a0921cccca2aafccd4ae7d80011f6104ad0
Carjooj <victor@4info.com.br>
2024-11-18 00:11:54 -0300

    feat: Criando arquivo com as definições de URL e URI da API

---
+++ assets/endpoints/Endpoints.ts
@@ -0,0 +1,11 @@
+const APILOCATION = "http://192.168.1.37:8080/"
+
+const APIBASE = "chromodoro/api/"
+
+const API = `${APILOCATION}${APIBASE}`
+
+export const UsuarioController = {
+    login: `${API}usuario/login`,
+    registrar: `${API}usuario/registrar`
+}
+
