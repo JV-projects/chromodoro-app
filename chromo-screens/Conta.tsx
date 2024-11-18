1be3df90d8b9e824b26d9ef49834dc72b319957d
Carjooj <victor@4info.com.br>
2024-11-18 00:14:02 -0300

    fix: removendo type e import sem utilização

--- chromo-screens/Conta.tsx
+++ chromo-screens/Conta.tsx
@@ -7,11 +7,6 @@ import { Text, View, StyleSheet } from "react-native";
 import Login from "./Login";
 import Cadastro from "./Cadastro";
 
-import { StackNavigationProp } from '@react-navigation/stack';
-
-type Conta = {
-    navigation: StackNavigationProp<any>
-}
 
 export default function Conta() {
 
