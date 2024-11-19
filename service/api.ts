b6dd3cc4d515cb09fec29fe10cfb01b282e92b60
Carjooj <victor@4info.com.br>
2024-11-19 00:26:39 -0300

    fix: Abrangendo respostas da API

--- service/api.ts
+++ service/api.ts
@@ -1,13 +1,11 @@
 import axios, {AxiosResponse} from 'axios'
 
-export async function post<T, R>(body: T, url: string, token?: string, ): Promise<R> {
+export async function post<T, R>(body: T, url: string, token?: string, ): Promise<AxiosResponse<R>> {
     try {
         const headers =
             token ? {'Content-Type': 'application/json', Authorization: `Bearer ${token}` } : {'Content-Type': 'application/json'};
 
-        const resposta: AxiosResponse<R> = await axios.post(url, body, { headers });
-
-        return resposta.data;
+        return await axios.post(url, body, {headers});
     } catch (erro: unknown) {
         if (axios.isAxiosError(erro)) {
             const mensagemErro = erro.response?.data || erro.message || "Erro desconhecido";
@@ -19,13 +17,11 @@ export async function post<T, R>(body: T, url: string, token?: string, ): Promis
     }
 }
 
-export async function get<R>(token: string, url: string): Promise<R> {
+export async function get<R>(token: string, url: string): Promise<AxiosResponse<R>> {
     try {
         const headers = token ? { Authorization: `Bearer ${token}` } : {};
 
-        const resposta: AxiosResponse<R> = await axios.get(url, { headers })
-
-        return resposta.data;
+        return await axios.get(url, {headers})
     } catch (erro: unknown) {
         if (axios.isAxiosError(erro)) {
             const mensagemErro = erro.response?.data || erro.message || "Erro desconhecido";
