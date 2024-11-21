053da0d1b3606393cdbdef01b3b5ebde7d067e37
Carjooj <victor@4info.com.br>
2024-11-21 00:07:32 -0300

    feat: Adicionando form de nova tarefa e coletando dados

--- components/Tarefa.tsx
+++ components/Tarefa.tsx
@@ -1,18 +1,22 @@
-import React from "react";
-import { View, StyleSheet, TouchableOpacity, Pressable, Text } from "react-native";
-import { Button } from "@rneui/themed";
+import React, {useState} from "react";
+import {View, StyleSheet, TouchableOpacity, Pressable, Text, TextInput} from "react-native";
+import {Button} from "@rneui/themed";
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 
-import { useEffect } from "react";
+import {useEffect} from "react";
+import {Icon} from "@rneui/base";
 
-export interface TarefaProps {
+export interface Tarefa {
     titulo: string;
-    totalCiclos: number;
     estCiclos: number;
-    status: "Em andamento" | "Concluída"
     descricao: string;
+}
+
+export interface TarefaProps extends Tarefa {
+    totalCiclos: number;
+    status: "Em andamento" | "Concluída"
 }
 
 interface TarefasArray {
@@ -22,20 +26,41 @@ interface TarefasArray {
 
 export default function Tarefa({ tarefas }: TarefasArray) {
 
+    const [exibeForm, setExibeForm] = useState<boolean>(false);
+    const [formTarefa, setFormTarefa] = useState<Tarefa>({ titulo: "", estCiclos: 0, descricao: "" })
 
-    useEffect(() => {
+    console.log(formTarefa)
 
+    const getData = (texto: string, prop: keyof Tarefa) => {
 
+        setFormTarefa((prevState) => ({...prevState, [prop]: texto}));
+    }
 
-    }, [])
+    const salvarTarefa = async () => {
+
+    }
 
     return (
         <View style={{gap: 20}}>
 
-            <TouchableOpacity style={styles.pressNovaTarefa}>
-                <Ionicons name="add-circle-outline" size={24} color="black" />
+            <TouchableOpacity style={styles.pressNovaTarefa} onPress={() => setExibeForm(true)}>
+                <Ionicons name="add-circle-outline" size={24} color="black"/>
                 <Text style={styles.pressText}>Nova tarefa</Text>
             </TouchableOpacity>
+
+            {exibeForm && (
+                <View style={styles.novaTarefa}>
+                    <View style={styles.inputContainer}>
+                        <TextInput onChangeText={(texto) => getData(texto, "titulo")} style={[styles.input, { width: '70%'}]} inputMode={"text"} placeholder={"Título da Tarefa"}/>
+                        <TextInput onChangeText={(texto) => getData(texto, "estCiclos")} style={[styles.input, { width: '20%'}]} keyboardType={"number-pad"} placeholder={"Est Ciclos"}/>
+                    </View>
+                    <TextInput onChangeText={(texto) => getData(texto, "descricao")} style={styles.input} inputMode={"text"} multiline={true} maxLength={500} placeholder={"Descrição"}/>
+                    <View style={{ flexDirection: 'row', alignSelf: 'flex-end', gap: 5}}>
+                        <Button onPress={() => setExibeForm(false)} title={"Cancelar"} color={"#D1717B"}/>
+                        <Button title={"Salvar"} color={"#D1717B"}/>
+                    </View>
+                </View>
+            )}
 
             {tarefas.map((tarefa, index) => (
                 <Pressable key={index} style={styles.pressable}>
@@ -47,10 +72,16 @@ export default function Tarefa({ tarefas }: TarefasArray) {
                         <Text>{tarefa.status}</Text>
                         <View style={styles.corpo}>
                             <View style={styles.viewDesc}>
-                                <Text style={{ fontSize: 13 }}>{tarefa.descricao}</Text>
+                                <Text style={{fontSize: 13}}>{tarefa.descricao}</Text>
                             </View>
                             <View style={styles.viewBotao}>
-                                <Button title="Excluir" />
+                                <Button
+                                    color={"#D1717B"}
+                                    icon={<Icon
+                                        name="delete"
+                                        size={24}
+                                        color="white"
+                                    />}/>
                             </View>
                         </View>
                     </View>
@@ -70,7 +101,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 8,
         height: 200,
-        
+
     },
     spacing: {
         flexDirection: 'row',
@@ -91,7 +122,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignSelf: 'flex-end'
     },
-    pressNovaTarefa:{
+    pressNovaTarefa: {
         width: '100%',
         marginTop: 10,
         height: 55,
@@ -105,7 +136,25 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         borderStyle: 'dashed'
     },
-    pressText:{
+    pressText: {
         fontSize: 16
+    },
+    novaTarefa: {
+        display: "flex",
+        backgroundColor: "#FCF2F0",
+        borderColor: "#D1717B",
+        padding: 15,
+        borderWidth: 1,
+        borderRadius: 8,
+        gap: 10
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    input: {
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderRadius: 5
     }
 })
