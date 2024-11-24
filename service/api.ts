c6195c86aeb036414cee58385cdd5a6fc22b9ce8
Carjooj <victor@4info.com.br>
2024-11-24 18:15:02 -0300

    feat: Adicionando método para request DELETE

--- service/api.ts
+++ service/api.ts
@@ -1,4 +1,5 @@
 import axios, {AxiosResponse} from 'axios'
+import {TarefaResponse} from "@/service/apiTypes";
 
 export async function post<T, R>(body: T, url: string, token?: string): Promise<AxiosResponse<R>> {
     try {
@@ -33,6 +34,24 @@ export async function get<R>(url: string, token?: string): Promise<AxiosResponse
         }
     }
 }
+
+export async function atualizar<T, R>(body: T, url: string, token?: string | undefined): Promise<AxiosResponse<R>> {
+    try {
+        const headers =
+            token ? {'Content-Type': 'application/json', Authorization: `Bearer ${token}` } : {'Content-Type': 'application/json'};
+
+        return await axios.patch(url, body, {headers});
+    } catch (erro: unknown) {
+        if (axios.isAxiosError(erro)) {
+            const mensagemErro = erro.response?.data || erro.message || "Erro desconhecido";
+
+            throw new Error(mensagemErro)
+        } else {
+            throw new Error("Erro inesperado")
+        }
+    }
+}
+
 
 export async function deletar<R>(url: string, id: string, token?: string): Promise<AxiosResponse<R>> {
 
